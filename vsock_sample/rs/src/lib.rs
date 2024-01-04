b6b9fd62dd8e67a7fe340eee3f63ec0b72d716a5
i-m-aditya <aditya.p@Aditya-P.local>
2024-01-04 13:53:44 +0530

    Debugging

--- vsock_sample/rs/src/lib.rs
+++ vsock_sample/rs/src/lib.rs
@@ -110,10 +110,15 @@ pub fn server(args: ServerArgs) -> Result<(), String> {
         let len = recv_u64(fd)?;
         let mut buf = [0u8; BUF_MAX_LEN];
         recv_loop(fd, &mut buf, len)?;
+        hello_world();
         println!(
             "{}",
             String::from_utf8(buf.to_vec())
                 .map_err(|err| format!("The received bytes are not UTF-8: {:?}", err))?
         );
     }
+}
+
+pub fn hello_world() {
+    println!("Hello, world!");
 }
